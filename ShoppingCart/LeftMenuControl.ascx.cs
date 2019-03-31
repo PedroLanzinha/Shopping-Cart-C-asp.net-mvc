ac681b2c5a7809b1222623a2c353d664d6282697
unknown <theitalianpeter@hotmail.com>
2019-03-31 23:44:36 +0100

    Small update commit

--- ShoppingCart/LeftMenuControl.ascx.cs
+++ ShoppingCart/LeftMenuControl.ascx.cs
@@ -9,6 +9,10 @@ public partial class LeftMenuControl : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if(!IsPostBack)
+        {
+            dlMenu.DataSource = DataAccess.selectQuery("SELECT * FROM CATEGORIES");
+            dlMenu.DataBind();
+        }
     }
 }
